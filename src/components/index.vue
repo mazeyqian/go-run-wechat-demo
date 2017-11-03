f5195c7a662741c2a32b22bb2a8c45165d2a2cc8
mazey <mazey@mazey.net>
2017-11-03 16:55:53 +0800

    引入jquery 不过有其他报错

---
+++ src/components/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 class="text-center"><a href="/">爬虫 - 展示筛选</a></h1>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+  data () {
+    return {
+      message: 'vue-spider'
+    }
+  },
+  methods: {
+    getTest () {
+      $.ajax({
+        type: 'get',
+        async: true,
+        url: 'http://spider-show.mazey.cn/interface/post.php',
+        dataType: 'jsonp',
+        jsonpCallback: 'callSuccess',
+        success: function (data) {
+          console.log(data)
+        },
+        error: function (data) {
+          console.log('error: jsonp')
+        }
+      })
+    }
+  }
+}
+</script>
