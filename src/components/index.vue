b929781e9f3ea0a843048c06f35b20fe37beedce
mazey <mazey@mazey.net>
2017-11-04 00:32:43 +0800

    成功获取数据并展示，继续做功能。

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,22 @@
  <template>
    <div>
+     <h1 class="text-center"><a href="/">爬虫 - 展示筛选</a></h1>
      {{ message }}
+     <div class="container">
+       <template v-for="post in posts">
+         <div class="row">
+           <div class="col-md-8">
+             <article>
+               <header>
+                 <h2 class="text-center" v-html="post[0]"></h2>
+               </header>
+               <div contenteditable="true" v-html="post[1]">
+               </div>
+             </article>
+           </div>
+         </div>
+       </template>
+     </div>
    </div>
  </template>
 
@@ -9,7 +25,13 @@
  export default {
    data () {
      return {
-       message: 'vue-spider'
+       message: 'vue-spider',
+       posts: [
+         [
+           '标题',
+           '内容'
+         ]
+       ]
      }
    },
    created () {
@@ -17,12 +39,14 @@
    },
    methods: {
      getTest () {
+       let self = this
        axios.post('http://spider-show.mazey.cn/interface/post.php', {
          firstName: 'Fred',
          lastName: 'Flintstone'
        })
          .then(function (response) {
            console.log(response.data)
+           self.posts = response.data
          })
          .catch(function (error) {
            console.log(error)
