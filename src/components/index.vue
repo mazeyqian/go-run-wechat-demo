bfdd1eb7f179ddd415d853b5516954c0af11f1ed
mazey <mazey@mazey.net>
2017-11-03 23:28:27 +0800

    axios使用成功

---
+++ src/components/index.vue
@@ -0,0 +1,33 @@
+ <template>
+   <div>
+     {{ message }}
+   </div>
+ </template>
+
+ <script>
+ import axios from 'axios'
+ export default {
+   data () {
+     return {
+       message: 'vue-spider'
+     }
+   },
+   created () {
+     this.getTest()
+   },
+   methods: {
+     getTest () {
+       axios.post('http://spider-show.mazey.cn/interface/post.php', {
+         firstName: 'Fred',
+         lastName: 'Flintstone'
+       })
+         .then(function (response) {
+           console.log(response.data)
+         })
+         .catch(function (error) {
+           console.log(error)
+         })
+     }
+   }
+ }
+ </script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false
+import index from './components/index.vue'
 
 /* eslint-disable no-new */
-new Vue({
+let mazey = new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  template: '<index/>',
+  components: { index }
 })
+
+Vue.use(mazey)
