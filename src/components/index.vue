c35d08664a57bd9ddd962d5138973cd3d00a015d
mazey <mazey@mazey.net>
2017-11-04 21:39:07 +0800

    能够成功提交

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,16 +2,19 @@
    <div>
      <h1 class="text-center"><a href="/">爬虫 - 展示筛选</a></h1>
      <div class="container">
-       <template v-for="post in posts">
+       <template v-for="(post, index) in posts">
          <div class="row">
            <div class="col-md-8">
              <article>
                <header>
-                 <h2 class="text-center" v-html="post.title"></h2>
+                 <h2 class="text-center" v-html="post.post_title"></h2>
                </header>
-               <div contenteditable="true" v-html="post.content">
+               <div contenteditable="true" v-html="post.post_content" :id="'content-' + post.post_id">
                </div>
              </article>
+           </div>
+           <div class="col-md-4">
+             <button class="btn btn-primary" @click="submitPost(post.post_id, index)">提交</button>
            </div>
          </div>
        </template>
@@ -25,23 +28,25 @@
    data () {
      return {
        message: 'vue-spider',
+       dbName: 'wp',
        posts: [
          {
-           title: '标题',
-           content: '内容'
+           post_id: '0',
+           post_title: '标题',
+           post_content: '内容'
          }
        ]
      }
    },
    created () {
-     this.getTest()
+     this.getPosts()
    },
    methods: {
-     getTest () {
+     getPosts () {
        let self = this
        axios.post('http://spider-show.mazey.cn/interface/post.php', {
-         firstName: 'Fred',
-         lastName: 'Flintstone'
+         firstName: 'Mazey',
+         lastName: 'Qian'
        })
          .then(function (response) {
            console.log(response.data)
@@ -50,6 +55,47 @@
          .catch(function (error) {
            console.log(error)
          })
+     },
+     submitPost (postId, index) {
+       let self = this
+       axios({
+         method: 'post',
+         url: 'http://spider-show.mazey.cn/interface/submit.php',
+         data: {
+           post_id: postId,
+           post_title: self.getTitleAndContent(postId)['post_title'],
+           post_content: self.getTitleAndContent(postId)['post_content']
+         },
+         transformRequest: [function (data) {
+           // Do whatever you want to transform the data
+           let ret = ''
+           for (let it in data) {
+             ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+           }
+           return ret
+         }]
+       })
+         .then(function (response) {
+           console.log(response.data)
+         })
+         .catch(function (error) {
+           console.log(error)
+         })
+       self.posts.splice(index, 1)
+       window.scrollTo(0, 0)
+     },
+     getTitleAndContent (postId) {
+       let arr = this.posts
+       let retArr = []
+       for (let i = 0, max = arr.length; i < max; i++) {
+         let obj = arr[i]
+         if (obj.post_id === postId) {
+           retArr['post_title'] = obj.post_title
+           retArr['post_content'] = obj.post_content
+           return retArr
+         }
+       }
+       return false
      }
    }
  }
